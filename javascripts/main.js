1d351bd75191068a40ea4712d8c453a476977389
Keith Dahlby <dahlbyk@gmail.com>
2015-02-09 02:29:54 -0600

    Exclude non-GitHub links

--- javascripts/main.js
+++ javascripts/main.js
@@ -40,7 +40,7 @@
         , url = gh && ('https://api.github.com/repos' + gh[1] + 'issues?labels=' + gh[2])
         , count = $(this).find('.count');
     
-      if (!count.length){
+      if (gh && !count.length){
         count = $('<span class="count"><img src="images/octocat-spinner-32.gif" /></span>').appendTo(this);
         $.getJSON(url, function(data){
           count.html(data && typeof data.length === 'number' ? data.length.toString() : '?');

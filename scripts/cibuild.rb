b453fe918b2c0c28711d5609334ce972ea0bd5e2
Brendan Forster <brendan@github.com>
2015-11-02 14:51:23 -0800

    make this runnable from the root of the repo

--- scripts/cibuild.rb
+++ scripts/cibuild.rb
@@ -1,15 +1,18 @@
 require 'safe_yaml'
 
 def check_folder
-  all_files =  Dir["../_data/projects/*"]
-  yaml_files =  Dir["../_data/projects/*.yml"]
+  # i'm lazy
+  root = File.expand_path("..", __dir__)
+
+  all_files =  Dir[root + "/_data/projects/*"]
+  yaml_files =  Dir[root + "/_data/projects/*.yml"]
 
   # i'm lazy, there's gotta be an easier way to do this!
   yaml_files.each { |f| all_files.delete f }
   other_files = all_files.count
 
   if (other_files > 0) then
-    puts "#{other_files} files in directory which are not YAML files!"
+    puts "#{other_files} files in directory which are not YAML files:"
     all_files.each { |f| puts " - " + f }
   end
 end
@@ -33,19 +36,21 @@ def verify_file (f)
   return [f, nil]
 end
 
+root = File.expand_path("..", __dir__)
+
 check_folder
 
-results = Dir["../_data/projects/*.yml"].map { |f| verify_file(f) }
+results = Dir[root + "/_data/projects/*.yml"].map { |f| verify_file(f) }
 
 files_with_errors = results.select { |file, error| error != nil }
 error_count = files_with_errors.count
 
-success = results.select { |file, error| error.nil? }
-                 .count
-
-puts "#{success} files processed"
+success = results.select { |file, error| error.nil? }.count
 
 if (error_count > 0) then
-  puts "#{error_count} files with errors!"
+  puts "#{success} files processed - #{error_count} errors found:"
   files_with_errors.each { |path, error| puts path + " - " + error }
+  exit -1
+else
+  puts "#{success} files processed - no errors found!"
 end

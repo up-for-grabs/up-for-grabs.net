df31970262e85852efba8b347533276b94ba0f0f
Brendan Forster <brendan@github.com>
2015-11-02 13:32:36 -0800

    a simple script to parse all the YAML in a file

---
+++ scripts/cibuild.rb
@@ -0,0 +1,51 @@
+require 'safe_yaml'
+
+def check_folder
+  all_files =  Dir["../_data/projects/*"]
+  yaml_files =  Dir["../_data/projects/*.yml"]
+
+  # i'm lazy, there's gotta be an easier way to do this!
+  yaml_files.each { |f| all_files.delete f }
+  other_files = all_files.count
+
+  if (other_files > 0) then
+    puts "#{other_files} files in directory which are not YAML files!"
+    all_files.each { |f| puts " - " + f }
+  end
+end
+
+def verify_file (f)
+  begin
+    contents = File.read(f)
+    yaml = YAML.load(contents, :safe => true)
+    name = yaml[:name]
+
+    # TODO: check for other values
+    # TODO:
+  rescue Psych::SyntaxError
+    error = "Unable to parse the contents of file"
+    return [f, error]
+  rescue
+    error = "Unknown exception for file: " + $!
+    return [f, error]
+  end
+
+  return [f, nil]
+end
+
+check_folder
+
+results = Dir["../_data/projects/*.yml"].map { |f| verify_file(f) }
+
+files_with_errors = results.select { |file, error| error != nil }
+error_count = files_with_errors.count
+
+success = results.select { |file, error| error.nil? }
+                 .count
+
+puts "#{success} files processed"
+
+if (error_count > 0) then
+  puts "#{error_count} files with errors!"
+  files_with_errors.each { |path, error| puts path + " - " + error }
+end

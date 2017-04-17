3bdc36d04faf4e9c54416b2c860945aaf545bf03
Brendan Forster <brendan@github.com>
2017-04-17 15:51:07 +1000

    enable eslint and some simple rules

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};

12c9e6fba6c473d806516f8d9d51945681a59918
Daniel McGrath <daniel.mcgrath9@gmail.com>
2018-02-21 00:14:39 -0500

    revert reverted pagination commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,26 +6,38 @@
 
 
 
+<<<<<<< HEAD
   var renderProjects = function (tags, currentPageNumber, tagsString, names) { 
 
   // var renderProjects = function (tags, names) {
+=======
+  var renderProjects = function (tags, currentPageNumber, tagsString) {
+
+  var renderProjects = function (tags, names) {
+>>>>>>> parent of 1f61b75... Revert "Pagination (#665) (#303)"
     projectsPanel.html(compiledtemplateFn({
       "projects": projectsSvc.get(tags, names),
       "tags": projectsSvc.getTags(),
+      "tagsString": tagsString,
       "popularTags": projectsSvc.getPopularTags(6),
       "selectedTags": tags,
 
+<<<<<<< HEAD
       "currentPageNumber": currentPageNumber,
+=======
+      "currentPageNumber": currentPageNumber
+>>>>>>> parent of 1f61b75... Revert "Pagination (#665) (#303)"
 
       "names": projectsSvc.getNames(),
       "selectedNames": names
+
     }));
 
     projectsPanel.find("select.tags-filter").chosen({
       no_results_text: "No tags found by that name.",
       width: "95%"
     }).val(tags).trigger('chosen:updated').change(function (e) {
-      window.location.href = "#/tags/" + encodeURIComponent(($(this).val() || ""));
+      window.location.href = "#/page/1/tags/" + encodeURIComponent(($(this).val() || ""));
     });
 
     projectsPanel.find("select.names-filter").chosen({
@@ -37,22 +49,47 @@
     });
   };
 
+  //we scroll to the first project when user swich page
+  var scrollUp = function () {
+      $('html, body').animate({
+        scrollTop: $("#projects-panel").offset().top
+      }, 1000);
+    };
+
   var app = $.sammy(function () {
     this.get("#/", function (context) {
-      renderProjects();
+        renderProjects("", 1, "");
     });
 
-    this.get("#/tags/", function (context) {
-      renderProjects();
+    this.get("#/page/:page/", function (context) {
+      var page = this.params["page"];
+      //alert("param page: " + page);
+      renderProjects("", page, "");
+      scrollUp();
     });
 
-    this.get("#/tags/:tags", function (context) {
+    this.get("#/page/:page/tags/", function (context) {
+      var page = this.params["page"];
+      //alert("param page: " + page);
+      renderProjects("", page, "");
+      scrollUp();
+    });
+
+    this.get("#/page/:page/tags/:tags", function (context) {
       var tags = (this.params["tags"] || "").toLowerCase().split(",");
+<<<<<<< HEAD
       var tagsString = this.params["tags"] || "";
       var page = this.params["page"];
+=======
+      var tagsString = this.params["tags"];
+      var page = this.params["page"];
+      //alert("param tags: " + tags + "\n" + "param page: " + page);
+>>>>>>> parent of 1f61b75... Revert "Pagination (#665) (#303)"
       renderProjects(tags, page, tagsString);
       scrollUp();
     });
+
+
 
     this.get("#/names/", function (context) {
       renderProjects();
@@ -62,6 +99,7 @@
       var names = (this.params["names"] || "").toLowerCase().split(",");
       renderProjects(null, names);
     });
+
   });
 
   var storage = (function (global) {
@@ -165,7 +203,7 @@
         tags.push($(this).data("tag"));
       });
       var tagsString = tags.join(",");
-      window.location.href = "#/tags/" + tagsString;
+      window.location.href = "#/page/1/tags/" + tagsString;
     });
 
     app.raise_errors = true;

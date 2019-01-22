e0c0d74c6cf6898079bb0727d522577e22bed5ca
Brendan Forster <github@brendanforster.com>
2019-01-22 16:12:30 -0400

    get eslint to match prettier checks

--- javascripts/main.js
+++ javascripts/main.js
@@ -225,13 +225,15 @@
 
     if (!gh) {
       count = $(
-        "<span class=\"count\" title=\"Issue count is only available for projects on GitHub.\">?</span>"
+        /* eslint-disable-next-line quotes */
+        '<span class="count" title="Issue count is only available for projects on GitHub.">?</span>'
       ).appendTo(a);
       return;
     }
 
     count = $(
-      "<span class=\"count\"><img src=\"images/octocat-spinner-32.gif\" /></span>"
+      /* eslint-disable-next-line quotes */
+      '<span class="count"><img src="images/octocat-spinner-32.gif" /></span>'
     ).appendTo(a);
     var cached = storage.get(gh[1]);
     if (

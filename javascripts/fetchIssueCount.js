7c9f2931eea212c625a94b988cf79b7cc1f120a3
Brendan Forster <github@brendanforster.com>
2019-07-01 12:02:55 -0300

    introduce new module and tests for issue count fetch work

---
+++ javascripts/fetchIssueCount.js
@@ -0,0 +1,175 @@
+/* eslint global-require: "off" */
+/* eslint block-scoped-var: "off" */
+
+// @ts-nocheck
+
+// required for loading into a NodeJS context
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
+
+define(['whatwg-fetch', 'promise-polyfill'], function() {
+  const { localStorage, fetch } = window;
+
+  function getValue(key) {
+    if (typeof localStorage !== 'undefined') {
+      return JSON.parse(localStorage.getItem(key));
+    }
+    return undefined;
+  }
+
+  function clearValue(key) {
+    if (typeof localStorage !== 'undefined') {
+      localStorage.removeItem(key);
+    }
+  }
+
+  function setValue(key, value) {
+    try {
+      if (typeof localStorage !== 'undefined') {
+        localStorage.setItem(key, JSON.stringify(value));
+      }
+    } catch (exception) {
+      if (
+        exception != QUOTA_EXCEEDED_ERR &&
+        exception != NS_ERROR_DOM_QUOTA_REACHED
+      ) {
+        throw exception;
+      }
+    }
+  }
+
+  function inspectRateLimitError(response) {
+    const rateLimited = response.headers.get('X-RateLimit-Remaining') === '0';
+    const rateLimitReset = response.headers.get('X-RateLimit-Reset');
+    if (rateLimited && rateLimitReset) {
+      const rateLimitResetAt = new Date(1000 * rateLimitReset);
+      return new Error(
+        'GitHub rate limit met. Reset at ' +
+          rateLimitResetAt.toLocaleTimeString()
+      );
+    }
+  }
+
+  function inspectGenericError(json, response) {
+    const { message } = json;
+    const errorMessage = message || response.statusText;
+    return new Error('Could not get issue count from GitHub: ' + errorMessage);
+  }
+
+  function fetchIssueCount(ownerAndName, label) {
+    const cached = getValue(ownerAndName);
+
+    const yesterday = new Date() - 1000 * 60 * 60 * 24;
+
+    if (cached && cached.date && new Date(cached.date) >= yesterday) {
+      return Promise.resolve(cached.count);
+    }
+
+    const perPage = 30;
+
+    // TODO: we're not extracting the trailing slash in `ownerAndName` when the
+    //       previous regex is passed in here. This would be great to cleanup
+    //       at some stage
+    const apiURL =
+      'https://api.github.com/repos' +
+      ownerAndName +
+      'issues?labels=' +
+      label +
+      '&per_page=' +
+      perPage;
+
+    const settings = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    };
+
+    if (cached && cached.etag) {
+      settings.headers = {
+        ...settings,
+        'If-None-Match': cached.etag,
+      };
+    }
+
+    return new Promise(function(resolve, reject) {
+      return fetch(apiURL, settings).then(
+        function(response) {
+          if (!response.ok) {
+            if (response.status === 304) {
+              // no content is returned in the 304 Not Modified response body
+              resolve(cached.count);
+              return;
+            }
+
+            clearValue(ownerAndName);
+
+            const rateLimitError = inspectRateLimitError(response);
+            if (rateLimitError) {
+              reject(rateLimitError);
+              return;
+            }
+
+            response.json().then(
+              function(json) {
+                reject(inspectGenericError(json, response));
+              },
+              function(error) {
+                reject(error);
+              }
+            );
+
+            return;
+          }
+
+          const etag = response.headers.get('ETag');
+          const linkHeader = response.headers.get('Link');
+
+          if (linkHeader) {
+            const lastPageMatch = /<([^<>]*?page=(\d*))>; rel="last"/g.exec(
+              linkHeader
+            );
+            if (lastPageMatch && lastPageMatch.length === 3) {
+              const lastPageCount = Number(lastPageMatch[2]);
+              const baseCount = perPage * (lastPageCount - 1);
+              const count = baseCount + '+';
+
+              setValue(ownerAndName, {
+                count,
+                etag,
+                date: new Date(),
+              });
+
+              resolve(count);
+              return;
+            }
+          }
+
+          response.json().then(
+            function(json) {
+              if (json && typeof json.length === 'number') {
+                const count = json.length;
+                setValue(ownerAndName, {
+                  count,
+                  etag,
+                  date: new Date(),
+                });
+
+                resolve(count);
+              }
+            },
+            function(error) {
+              reject(error);
+            }
+          );
+        },
+        function(error) {
+          reject(error);
+        }
+      );
+    });
+  }
+
+  return fetchIssueCount;
+});

ca766c904611cdeb29f906af4666dd9f2b60360c
Brendan Forster <github@brendanforster.com>
2019-10-15 06:44:00 -0300

    warn about orphaned files during CI (#1560)

--- scripts/cibuild.rb
+++ scripts/cibuild.rb
@@ -19,15 +19,42 @@ def check_folder(root)
 
   count = other_files.count
 
+  if count.positive?
+    puts "#{count} files found in projects directory which are not YAML files:"
+    r = Pathname.new(root)
+
+    other_files.each do |f|
+      relative_path = Pathname.new(f).relative_path_from(r).to_s
+      puts " - #{relative_path}"
+    end
+
+    exit(-1)
+  end
+
+  valid_yaml_files = ['_config.yml', 'docker-compose.yml', '.rubocop.yml']
+
+  Find.find("#{root}/") do |path|
+    next unless FileTest.file?(path)
+    next unless File.dirname(path) == root
+
+    basename = File.basename(path)
+    next if valid_yaml_files.include?(basename)
+
+    other_files << basename if File.extname(path) == '.yml'
+  end
+
+  count = other_files.count
+
   return unless count.positive?
 
-  puts "#{count} files found in projects directory which are not YAML files:"
+  puts "#{count} files found in root which look like content files:"
   r = Pathname.new(root)
 
   other_files.each do |f|
-    relative_path = Pathname.new(f).relative_path_from(r).to_s
-    puts " - #{relative_path}"
+    puts " - #{f}"
   end
+
+  puts 'Move these inside _data/projects to ensure they are listed on the site'
 
   exit(-1)
 end

65bfd90393582a9e4f90c931724b8c908b419c25
Brendan Forster <github@brendanforster.com>
2019-11-14 16:33:12 -0400

    revert this to a token that might be available

--- .github/actions/project-analyzer/project_analyzer.rb
+++ .github/actions/project-analyzer/project_analyzer.rb
@@ -248,7 +248,7 @@ http = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
   def headers(_context)
     {
       "User-Agent": 'up-for-grabs-graphql-label-queries',
-      "Authorization": "bearer #{ENV['SHIFTBOT_TOKEN']}"
+      "Authorization": "bearer #{ENV['GITHUB_TOKEN']}"
     }
   end
 end

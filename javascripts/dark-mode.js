f455c62c63d5591a0378dd6dad78e73573b41f46
Brendan Forster <github@brendanforster.com>
2022-03-03 17:58:51 -0400

    extract logic for handling dark/light theming

--- javascripts/dark-mode.js
+++ javascripts/dark-mode.js
@@ -6,7 +6,44 @@ if (typeof define !== 'function') {
 }
 
 define([], () => {
-  let condition = true;
+  const root = document.documentElement;
+
+  /**
+   * Apply changes to document to put the page into "dark" mode
+   *
+   * @param {HTMLElement} viewModeElement
+   * @param {HTMLElement} viewModeAnchor
+   */
+  function setDarkMode(viewModeElement, viewModeAnchor) {
+    root.style.setProperty('--body-back', '#111111');
+    root.style.setProperty('--body-color', '#eeeded');
+    root.style.setProperty('--abs', 'rgb(37, 37, 37)');
+
+    viewModeElement.setAttribute('src', '/images/sun-light.png');
+    viewModeAnchor.title = 'light-mode';
+  }
+
+  /**
+   * Apply changes to document to put the page into "light" mode
+   *
+   * @param {HTMLElement} viewModeElement
+   * @param {HTMLElement} viewModeAnchor
+   */
+  function setLightMode(viewModeElement, viewModeAnchor) {
+    root.style.setProperty('--body-back', '#f9f9f9');
+    root.style.setProperty('--body-color', '#303030');
+    root.style.setProperty('--abs', '#FFF');
+
+    viewModeElement.setAttribute('src', '/images/Dim-Night.png');
+    viewModeAnchor.title = 'dark-mode';
+  }
+
+  /**
+   * @param {"dark" | "light"} value
+   */
+  function storeValue(value) {
+    window.sessionStorage.setItem('mode', value);
+  }
 
   function setupDarkModeListener() {
     var r = document.querySelector(':root');

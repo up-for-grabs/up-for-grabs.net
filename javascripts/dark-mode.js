7e7247ccbf2cf196b52437905a343c2c10d2dc6b
Brendan Forster <github@brendanforster.com>
2022-03-03 17:33:09 -0400

    defer execution of listener wireup til later

--- javascripts/dark-mode.js
+++ javascripts/dark-mode.js
@@ -1,7 +1,5 @@
 /* eslint block-scoped-var: "off" */
 
-/// <reference types="node" />
-
 // required for loading into a NodeJS context
 if (typeof define !== 'function') {
   var define = require('amdefine')(module);
@@ -10,38 +8,42 @@ if (typeof define !== 'function') {
 define([], () => {
   let condition = true;
 
-  var r = document.querySelector(':root');
-  if (!r) {
-    return;
-  }
-
-  const viewModeElement = document.getElementById('view-mode');
-  if (!viewModeElement) {
-    return;
-  }
-
-  const viewModeAnchor = document.getElementById('view-mode-a');
-  if (!viewModeAnchor) {
-    return;
-  }
-
-  const root = document.documentElement;
-
-  viewModeAnchor.addEventListener('click', () => {
-    if (condition) {
-      root.style.setProperty('--body-back', '#111111');
-      root.style.setProperty('--body-color', '#eeeded');
-      root.style.setProperty('--abs', 'rgb(37, 37, 37)');
-      condition = false;
-      viewModeElement.setAttribute('src', '/images/sun-light.png');
-      viewModeAnchor.title = 'light-mode';
-    } else {
-      root.style.setProperty('--body-back', '#f9f9f9');
-      root.style.setProperty('--body-color', '#303030');
-      root.style.setProperty('--abs', '#FFF');
-      condition = true;
-      viewModeElement.setAttribute('src', '/images/Dim-Night.png');
-      viewModeAnchor.title = 'dark-mode';
+  function setupDarkModeListener() {
+    var r = document.querySelector(':root');
+    if (!r) {
+      return;
     }
-  });
+
+    const viewModeElement = document.getElementById('view-mode');
+    if (!viewModeElement) {
+      return;
+    }
+
+    const viewModeAnchor = document.getElementById('view-mode-a');
+    if (!viewModeAnchor) {
+      return;
+    }
+
+    const root = document.documentElement;
+
+    viewModeAnchor.addEventListener('click', () => {
+      if (condition) {
+        root.style.setProperty('--body-back', '#111111');
+        root.style.setProperty('--body-color', '#eeeded');
+        root.style.setProperty('--abs', 'rgb(37, 37, 37)');
+        condition = false;
+        viewModeElement.setAttribute('src', '/images/sun-light.png');
+        viewModeAnchor.title = 'light-mode';
+      } else {
+        root.style.setProperty('--body-back', '#f9f9f9');
+        root.style.setProperty('--body-color', '#303030');
+        root.style.setProperty('--abs', '#FFF');
+        condition = true;
+        viewModeElement.setAttribute('src', '/images/Dim-Night.png');
+        viewModeAnchor.title = 'dark-mode';
+      }
+    });
+  }
+
+  return setupDarkModeListener;
 });
